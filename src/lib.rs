6913bd61f9208162d61b20295e7c958c057dccab
Bammez <Kingnuel4@gmail.com>
2024-05-20 17:43:44 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {n}")
+    let mut num = n;
+    let mut factors = Vec::new();
+    let mut divisor = 2;
+
+    while num > 1 {
+        while num % divisor == 0 {
+            factors.push(divisor);
+            num /= divisor;
+        }
+        divisor += 1;
+        // If divisor reaches the square root of num and num is still greater than 1,
+        // then num must be a prime number greater than the divisor and should be added to the factors
+        if divisor * divisor > num && num > 1 {
+            factors.push(num);
+            break;
+        }
+    }
+
+    factors
 }
+
+
